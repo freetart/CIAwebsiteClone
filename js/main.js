e4f5b93fb79f6d78ad342994d3d6beb843d7c145
Chris M <habibdev96@gmail.com>
2021-03-05 17:54:04 -0700

    created navbar

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
 "use strict";
+
+// add background to nav on scroll
+const nav = document.querySelector(".navbar");
+
+function fixedNav() {
+  if (window.scrollY > nav.offsetHeight + 1) {
+    nav.classList.add("navbarScroll");
+  } else {
+    nav.classList.remove("navbarScroll");
+  }
+}
+
+window.addEventListener("scroll", fixedNav);
